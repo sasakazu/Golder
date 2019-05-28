6b551b4152c9398f0881901f41cc032a53faaa16
笹倉一也 <sasakurakazuya@sasakurakazuyanoimac.local>
2019-05-28 20:37:05 +0900

    Initial Commit

---
+++ Golden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Golden
+//
+//  Created by 笹倉一也 on 2019/05/28.
+//  Copyright © 2019年 笹倉一也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
