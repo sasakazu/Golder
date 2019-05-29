0336336706bb55a97ae1747f772c47911b3d0313
kazuyasasakura <kazuya.sasakura@gmial.com>
2019-05-29 18:06:16 +0900

    image picker controller done....

--- Golden/みんなのゴールデン/addCollecton.swift
+++ Golden/みんなのゴールデン/addCollecton.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class addCollecton: UIViewController {
+class addCollecton: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     
     @IBOutlet weak var imageView: UIImageView!
@@ -22,11 +22,117 @@ class addCollecton: UIViewController {
     
     @IBAction func camera(_ sender: Any) {
         
-    }
-    
- 
-    @IBAction func save(_ sender: Any) {
+        let sourceType:UIImagePickerController.SourceType =
+            UIImagePickerController.SourceType.camera
+        // カメラが利用可能かチェック
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerController.SourceType.camera){
+            // インスタンスの作成
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self
+            self.present(cameraPicker, animated: true, completion: nil)
+            
+        }
+        else{
+            print("error")
+            
+        }
         
     }
+    
+    @IBAction func alubum(_ sender: Any) {
+        
+        let sourceType:UIImagePickerController.SourceType =
+            UIImagePickerController.SourceType.photoLibrary
+        
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerController.SourceType.photoLibrary){
+            // インスタンスの作成
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self
+            self.present(cameraPicker, animated: true, completion: nil)
+            
+            
+        }
+            
+        else{
+        
+            
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    //　撮影が完了時した時に呼ばれる
+    func imagePickerController(_ imagePicker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        
+        if let pickedImage = info[.originalImage]
+            as? UIImage {
+            
+            imageView.image = pickedImage
+            
+        }
+        
+        //閉じる処理
+        imagePicker.dismiss(animated: true, completion: nil)
+      
+        
+    }
+    
+    // 撮影がキャンセルされた時に呼ばれる
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+        print("Canceled")
+    }
+ 
+    // 書き込み完了結果の受け取り
+    @objc func image(_ image: UIImage,
+                     didFinishSavingWithError error: NSError!,
+                     contextInfo: UnsafeMutableRawPointer) {
+        
+        if error != nil {
+            print(error.code)
+
+        }
+        else{
+
+        }
+    }
+    
+    
+    
+    @IBAction func save(_ sender: Any) {
+        
+        
+        
+//        let image:UIImage! = imageView.image
+//
+//        if image != nil {
+//            UIImageWriteToSavedPhotosAlbum(
+//                image,
+//                self,
+//                #selector(addCollecton.image(_:didFinishSavingWithError:contextInfo:)),
+//                nil)
+//        }
+//        else{
+//
+//        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
